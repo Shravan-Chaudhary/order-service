ebf8893c342a9466717f8868d9a030cef4815b7c
Shravan-Chaudhary <shravansinghchaudhary@gmail.com>
2024-09-03 00:17:15 +0530

    feat: :wrench: Created httpResponse utility function

---
+++ src/utils/httpResponse.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import { THttpResponse } from "../types";
+import Config from "../config";
+import { EApplicationEnvoirnment } from "../constants/application";
+
+const httpResponse = (
+    req: Request,
+    res: Response,
+    responseStatusCode: number,
+    responseMessage: string,
+    data: unknown = null
+): void => {
+    const response: THttpResponse = {
+        success: true,
+        statusCode: responseStatusCode,
+        request: {
+            ip: req.ip ?? null,
+            method: req.method,
+            url: req.originalUrl
+        },
+        message: responseMessage,
+        data: data
+    };
+
+    // Log
+    console.info("Controller_response", {
+        meta: response
+    });
+
+    // Remove IP if in production
+    if (Config.ENV === EApplicationEnvoirnment.PRODUCTION) {
+        delete response.request.ip;
+    }
+
+    res.status(responseStatusCode).json(response);
+};
+
+export default httpResponse;
+
