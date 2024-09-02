a61fff4626fe2fd1e9fb8f98d90dcdfa7c85921d
Shravan-Chaudhary <shravansinghchaudhary@gmail.com>
2024-09-03 00:12:49 +0530

    feat: :rocket: Start application in server file

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,22 @@
-console.log('Hello World')
+import app from "./app";
+import Config from "./config";
+
+const startServer = () => {
+    const PORT = Config.PORT ?? 5501;
+
+    try {
+        app.listen(PORT, () => {
+            console.info("Server started", {
+                PORT: PORT,
+                SERVER_URL: Config.SERVER_URL
+            });
+        });
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(error);
+            process.exit(1);
+        }
+    }
+};
+
+void startServer();
