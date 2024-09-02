face7f88db7c029a24e45cf331ad06f790e15f65
Shravan-Chaudhary <shravansinghchaudhary@gmail.com>
2024-09-03 00:13:58 +0530

    feat: :toolbox: Add middlewares in app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response, Application } from "express";
+import path from "path";
 
 const app: Application = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (_req: Request, res: Response) => {
     res.send("Hello world");
