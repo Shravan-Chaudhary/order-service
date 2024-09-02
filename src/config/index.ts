783abab559c10881ab2f20d9f3680ad3a0b7c901
Shravan-Chaudhary <shravansinghchaudhary@gmail.com>
2024-09-03 00:14:45 +0530

    refactor: :recycle: Refactor config export

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,9 +2,11 @@ import dotenvFlow from "dotenv-flow";
 
 dotenvFlow.config();
 
-export default {
+const Config = {
     ENV: process.env.ENV,
     PORT: process.env.PORT,
     SERVER_URL: process.env.SERVER_URL
 };
+
+export default Config;
 
