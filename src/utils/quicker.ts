77f214c45fe66120a1d7d3a199eed1201ed9f9ca
Shravan <shravansinghchaudhary@gmail.com>
2024-09-22 17:52:25 +0530

    feat: :sparkles: Add system and application health check functions

---
+++ src/utils/quicker.ts
@@ -0,0 +1,21 @@
+import os from "os";
+import Config from "../config";
+
+export const getSystemHealth = () => {
+    return {
+        cpuUsage: os.loadavg(),
+        totalMemory: `${(os.totalmem() / 1024 / 1024).toFixed()} MB`,
+        freeMemory: `${(os.freemem() / 1024 / 1024).toFixed()} MB`
+    };
+};
+
+export const getApplicationHealth = () => {
+    return {
+        environment: Config.ENV,
+        upTime: `${process.uptime().toFixed(2)} Seconds`,
+        memoryUsage: {
+            heapTotal: `${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed()} MB`,
+            heapUsed: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed()} MB`
+        }
+    };
+};
