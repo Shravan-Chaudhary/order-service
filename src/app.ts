d1e66a0b19456188a19a15d82b4aa3752e4a6732
Shravan <shravansinghchaudhary@gmail.com>
2024-09-22 17:53:02 +0530

    feat: :heavy_plus_sign: Add helmet as middleware

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,14 @@ import { globalErrorHandler } from "./middlewares/globalErrorHandler";
 import httpResponse from "./utils/httpResponse";
 import HttpStatus from "./utils/httpCodes";
 import ResponseMessage from "./constants/responseMessage";
+import CreateHttpError from "./utils/httpErrors";
+import { getApplicationHealth, getSystemHealth } from "./utils/quicker";
+import helmet from "helmet";
 
 const app: Application = express();
 
 // Middlewares
+app.use(helmet());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 
@@ -18,14 +22,35 @@ app.get("/", (req: Request, res: Response, _next: NextFunction) => {
     });
 });
 
+// Health check
+app.get(
+    "/api/v1/health",
+    (req: Request, res: Response, _next: NextFunction) => {
+        const healthData = {
+            application: getApplicationHealth(),
+            system: getSystemHealth(),
+            timeStamp: Date.now()
+        };
+
+        httpResponse(
+            req,
+            res,
+            HttpStatus.OK,
+            ResponseMessage.SUCCESS,
+            healthData
+        );
+    }
+);
+
 // 404 handler
-// app.use((req: Request, _: Response, next: NextFunction) => {
-//     try {
-
-//     } catch (error) {
-
-//     }
-// })
+app.use((_req: Request, _res: Response, next: NextFunction) => {
+    try {
+        const error = CreateHttpError.NotFoundError("Route not found");
+        throw error;
+    } catch (error) {
+        next(error);
+    }
+});
 
 // Global error handler
 app.use(globalErrorHandler);
