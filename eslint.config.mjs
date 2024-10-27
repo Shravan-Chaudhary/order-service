a64c92e62141873316ac2e3b9a0ed0c4b07ec0e5
Shravan <shravansinghchaudhary@gmail.com>
2024-10-28 05:20:06 +0530

    refactor: Update eslint.config.mjs with rule changes and ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 // @ts-check
-
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
@@ -22,6 +21,21 @@ export default tseslint.config({
     rules: {
         "no-console": "error",
         "no-useless-catch": 0,
-        quotes: ["error", "double", { allowTemplateLiterals: true }]
-    }
+        "@typescript-eslint/no-misused-promises": "off",
+        "@typescript-eslint/ban-ts-comment": "off",
+        quotes: ["error", "double", { allowTemplateLiterals: true }],
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            {
+                args: "all",
+                argsIgnorePattern: "^_",
+                caughtErrors: "all",
+                caughtErrorsIgnorePattern: "^_",
+                destructuredArrayIgnorePattern: "^_",
+                varsIgnorePattern: "^_",
+                ignoreRestSiblings: true
+            }
+        ]
+    },
+    ignores: ["node_modules", "dist", "**/*.spec.ts", "tests"]
 });
