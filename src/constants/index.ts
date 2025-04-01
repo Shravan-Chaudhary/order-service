ef1af5db3c266afd2b5ae57f337b09e96339d01e
Shravan <shravansinghchaudhary@gmail.com>
2025-04-01 13:40:59 +0530

    feat: :sparkles: Add constants for time and widget/price types

---
+++ src/constants/index.ts
@@ -0,0 +1,11 @@
+export const ONE_HOUR_IN_SEC = 3600;
+
+export enum WidgetType {
+    SWITCH = "switch",
+    RADIO = "radio"
+}
+
+export enum PriceType {
+    BASE = "base",
+    ADDITIONAL = "additional"
+}
