56fe40893e65ba8eb04fea4e5cec3db319d42e56
Shravan <shravansinghchaudhary@gmail.com>
2025-04-01 13:42:00 +0530

    feat: :sparkles: Add types for Topping and Product events

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,6 @@
+import mongoose from "mongoose";
+import { PriceType } from "../constants";
+
 export interface AuthCookie {
     accessToken: string;
 }
@@ -12,6 +15,30 @@ export interface AuthRequest extends Request {
         email: string;
     };
 }
+// export interface OrderRequest extends Request {
+//     body: {
+//         cart: CartItem[];
+//     };
+// }
+export enum ToppingEvents {
+    TOPPING_CREATE = "TOPPING_CREATE",
+    TOPPING_UPDATE = "TOPPING_UPDATE",
+    TOPPING_DELETE = "TOPPING_DELETE"
+}
+export interface ToppingMessage {
+    event_type: ToppingEvents;
+    data: {
+        id: string;
+        price: number;
+        tenantId: string;
+    };
+}
+export interface ToppingPriceCache {
+    _id: mongoose.Types.ObjectId;
+    toppingId: string;
+    price: number;
+    tenantId: string;
+}
 
 export interface PriceConfiguration {
     priceType: "base" | "additional";
@@ -24,7 +51,52 @@ export interface ProductPricingCache {
     priceConfiguration: PriceConfiguration;
 }
 
+export enum ProductEvents {
+    PRODUCT_CREATE = "PRODUCT_CREATE",
+    PRODUCT_UPDATE = "PRODUCT_UPDATE",
+    PRODUCT_DELETE = "PRODUCT_DELETE"
+}
 export interface ProductMessage {
-    id: string;
-    priceConfiguration: PriceConfiguration;
+    event_type: ProductEvents;
+    data: {
+        _id: string;
+        priceConfiguration: PriceConfiguration;
+    };
+}
+
+export interface ProductPriceConfiguration {
+    [key: string]: {
+        priceType: PriceType.BASE | PriceType.ADDITIONAL;
+        availableOptions: {
+            [key: string]: number;
+        };
+    };
+}
+
+export type Topping = {
+    _id: string;
+    name: string;
+    image: string;
+    price: number;
+    isAvailable: boolean;
+};
+
+export type Product = {
+    _id: string;
+    name: string;
+    description: string;
+    image: string;
+    priceConfiguration: ProductPriceConfiguration;
+};
+
+export interface CartItem
+    extends Pick<Product, "_id" | "name" | "image" | "priceConfiguration"> {
+    selectedConfig: {
+        selectedPriceConfig: {
+            [key: string]: string;
+        };
+        selectedToppings: Topping[];
+    };
+    qty: number;
+    hash?: string;
 }
