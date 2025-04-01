83f256d322faf41b2c18da6842851ad6b685ec22
Shravan <shravansinghchaudhary@gmail.com>
2025-04-01 13:42:38 +0530

    feat: :sparkles: Refactor product update handling to improve JSON parsing and update logic

--- src/modules/productCache/productCacheHandler.ts
+++ src/modules/productCache/productCacheHandler.ts
@@ -2,23 +2,24 @@ import { ProductMessage } from "../../types";
 import productCacheModel from "./productCacheModel";
 
 export const handleProductUpdate = async (value: string) => {
+    let product: ProductMessage;
     try {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        const product: ProductMessage = JSON.parse(value);
-        return await productCacheModel.updateOne(
-            {
-                productId: product.id
-            },
-            {
-                $set: {
-                    priceConfiguration: product.priceConfiguration
-                }
-            },
-            // if record not found, create new one. if found, update it
-            { upsert: true }
-        );
+        product = JSON.parse(value);
     } catch (_error) {
         const err = new Error("Parsing JSON error");
         throw err;
     }
+    return await productCacheModel.updateOne(
+        {
+            productId: product.data._id
+        },
+        {
+            $set: {
+                priceConfiguration: product.data.priceConfiguration
+            }
+        },
+        // if record not found, create new one. if found, update it
+        { upsert: true }
+    );
 };
